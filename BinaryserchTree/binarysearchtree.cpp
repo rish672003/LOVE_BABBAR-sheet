f58c9550a1e796933a1914b1a0bf69a83b9d99fc
Rishabh Sharma <rishabhshrama8734@gmail.com>
2025-01-11 13:11:44 +0530

    learning binary tree

--- BinaryserchTree/binarysearchtree.cpp
+++ BinaryserchTree/binarysearchtree.cpp
@@ -15,40 +15,40 @@ class Node{
     }
 
 };
-void levelOrdertraversal(Node *root)
-{
-    queue<Node *> q;
-    q.push(root);
-    q.push(NULL);
+// void levelOrdertraversal(Node *root)
+// {
+//     queue<Node *> q;
+//     q.push(root);
+//     q.push(NULL);
 
-    while (!q.empty())
-    {
-        Node *temp = q.front();
-        q.pop();
+//     while (!q.empty())
+//     {
+//         Node *temp = q.front();
+//         q.pop();
 
-        if (temp == NULL)
-        {
-            cout << endl;
-            if (!q.empty())
-            {
-                q.push(NULL);
-            }
-        }
-        else
-        {
-            cout << temp->data << " ";
+//         if (temp == NULL)
+//         {
+//             cout << endl;
+//             if (!q.empty())
+//             {
+//                 q.push(NULL);
+//             }
+//         }
+//         else
+//         {
+//             cout << temp->data << " ";
 
-            if (temp->left)
-            {
-                q.push(temp->left);
-            }
-            if (temp->right)
-            {
-                q.push(temp->right);
-            }
-        }
-    }
-}
+//             if (temp->left)
+//             {
+//                 q.push(temp->left);
+//             }
+//             if (temp->right)
+//             {
+//                 q.push(temp->right);
+//             }
+//         }
+//     }
+// }
 
 Node* insertintoBST(Node * root, int d){
 
@@ -77,15 +77,40 @@ void takeinput(Node *&root){
             cin>> data;
         }
         
-
-
 };
+
+
+//minimum value if the BST
+Node* minValu(Node* root){
+    Node* temp = root;
+    while(temp->left = NULL){
+            temp = temp ->left;
+        
+    }
+    return temp;
+    
+    
+}
+Node* maxValu(Node* root){
+    Node* temp = root;
+    while(temp->right != NULL){
+            temp = temp->right;
+        
+    }
+    return temp;
+    
+    
+}
 
 int main(){
     Node* root  = NULL;
 
     cout<< "Enter data to create BST: ";
+
     takeinput(root);
-    levelOrdertraversal(root);
+    // levelOrdertraversal(root);
+    cout << " MIN value is " << minValu(root)->data<<endl;;
+    cout << " Max value is " << maxValu(root)->data;
+    
     return 0;
 }
