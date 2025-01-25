e3a0cf9577ab226ce4fb2c83aef6ede0101ff10a
Rishabh Sharma <rishabhsharma8734@gmail.com>
2025-01-25 15:23:14 +0530

    question no1

---
+++ q_1reverseastring.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+    string reverseString(string& s) {
+        // code here
+    int left = 0;  int right = s.size() -1;
+    
+    while(left< right){
+        swap(s[left],s[right]);
+        left++;
+        right--;
+    }
+    
+    return s;
+        
+        
+    }
+};
