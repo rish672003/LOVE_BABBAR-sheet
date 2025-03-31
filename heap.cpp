c3551c80c88a0361dec8b30d43b43ec535496d42
Rishabh Sharma <rishabhsharma8734@gmail.com>
2025-03-31 22:36:50 +0530

    finish heap

--- heap.cpp
+++ heap.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#include "heap.h" 
 
 using namespace std;
 
@@ -74,6 +75,57 @@ public:
     }
 
 
+    // void heapify(int arr[], int n, int i ){
+    //     int largest = i;
+    //     int left = 2*i;
+    //     int right = 2*i+1;
+
+    //     if(left<n && arr[largest]<arr[left]){
+    //         largest = left;
+    //     }
+
+    //     if(right<n && arr[largest]< arr[right]){
+    //         largest = right;
+    //     }
+
+    //     if(largest!=i){
+    //         swap(arr[largest], arr[i]);
+    //         heapify(arr,n,largest);
+    //     }
+
+
+
+
+    // }
+
+
+void heapify(int arr[],int n, int i){
+        int largest = i;
+        int left = 2*i;
+        int right = 2*i+1;
+  
+        if(left<n && arr[largest]<arr[left]  ){
+            largest = left;
+        }
+
+        if(right<n && arr[largest]<arr[right]){
+            largest = right;
+        }
+
+        if(largest != 1){
+      
+      
+            swap(arr[largest],arr[i]);
+                heapify(arr,n,largest);
+        }
+
+
+
+    }
+
+
+
+
 
 
 
@@ -90,6 +142,21 @@ int main()
 
     h.deletefromHeap();
     h.print();
+
+    int arr[6] = {-1,54,53,55,52,50};
+
+    int n = 5;
+    for(int i= n/2; i>0; i--){
+        heapify(arr,n,i);
+    }
+    cout<<"printing the arr now" << endl;
+
+    for(int i =1;i<n;i++){
+        cout<<arr[i]<<" ";
+
+    }cout<<endl;
+
+
 
 
     return 0;
---
+++ web dev/index.js
@@ -0,0 +1,8 @@
+
+console.log("hello world");
+
+function myfunction(){
+    document.getElementsByClassName("demo").innerHTML = "Hey This is the Start";
+}
+
+console.log(myfunction());
